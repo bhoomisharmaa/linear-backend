e7c858a9392de7487af67ac0a80436a0ba7fd5df
bhoomisharmaa <bhoomisharma404@gmail.com>
2024-06-20 23:52:38 +0530

    shimmyy shimmyyy

--- dist/index.js
+++ dist/index.js
@@ -4,20 +4,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const issues_1 = __importDefault(require("./routes/issues"));
+const team_1 = __importDefault(require("./routes/team"));
+const client_1 = require("@prisma/client");
 const app = (0, express_1.default)();
+const prisma = new client_1.PrismaClient();
 const cors = require("cors");
-const issueRouter = require("./issues");
-const teamRouter = require("./team");
 app.use(cors({
     origin: "http://localhost:3000",
 }));
 app.use(express_1.default.json());
-app.use("/issues", issueRouter);
-app.use("/teams", teamRouter);
+app.use("/issues", issues_1.default);
+app.use("/teams", team_1.default);
 app.get("/", (req, res) => {
     res.send("Server is running");
 });
 app.listen(3001, () => {
     console.log(`Server is running on port 3001`);
 });
+async function getWorkspaceName() {
+    try {
+        const work = await prisma.workspace.findMany({
+            where: {
+                workName: "Tierra",
+            },
+        });
+    }
+    catch (error) {
+        console.log("Error creating workshop", error);
+    }
+    finally {
+        await prisma.$disconnect();
+    }
+}
+app.get("/get-workspace");
 module.exports = app;
