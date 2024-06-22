539c901054a26315225f992b8dd1b1a9d3463f22
bhoomisharmaa <bhoomisharma404@gmail.com>
2024-06-22 14:34:04 +0530

    port

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,9 @@ app.use("/teams", teamRouter);
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-app.listen(process.env.PORT || 3001, () => {
-  console.log(`Server is running on port 3001`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 async function getWorkspaceName() {
