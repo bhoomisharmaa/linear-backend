850a85b8c19f9274f56eaab008ddc644ac8d01af
bhoomisharmaa <bhoomisharma404@gmail.com>
2024-06-22 14:09:10 +0530

    port

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use("/teams", teamRouter);
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log(`Server is running on port 3001`);
 });
 
